9465963ba7714460ca21d94001d115250a59c585
Dan Kercher <dankercher@gmail.com>
2025-11-13 12:35:18 -0500

    adjust font size for mobile

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -17,7 +17,7 @@ export function Section({
       <div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
         <h2
           id={id}
-          className="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
+          className="text-2xl md:text-base font-semibold text-zinc-800 dark:text-zinc-100"
         >
           {title}
         </h2>
