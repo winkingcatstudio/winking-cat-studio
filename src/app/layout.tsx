b271a8edc913cbfc0cad6fa9ce20d7996aecfef0
Dan Kercher <dankercher@gmail.com>
2025-11-20 11:02:55 -0500

    control link preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,27 @@ export const metadata: Metadata = {
     template: '%s - Dan Kercher',
     default: 'Winking Cat Studio - Software Development and Game Design',
   },
-  description: `Hi! I'm Dan Kercher, a software developer and tabletop roleplaying game designer located in Indianapolis, Indiana. I started Winking Cat Studio as a way to organize my personal, academic, and professional projects.`,
+  description: `Hi! I'm Dan Kercher, a software developer and tabletop roleplaying game designer. I started Winking Cat Studio as a way to organize my personal, academic, and professional projects.`,
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon.ico',
     apple: '/favicon.png',
   },
-}
+  openGraph: {
+    title: 'Winking Cat Studio - Software Development and Game Design',
+    description: `Hi! I'm Dan Kercher, a software developer and tabletop roleplaying game designer.`,
+    url: 'https://winkingcatstudio.com',
+    siteName: 'Winking Cat Studio',
+    images: [
+      {
+        url: 'https://winkingcatstudio.com/images/winkingcatlogo-white-on-black.png',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    type: 'website',
+  },
+};
 
 export default function RootLayout({
   children,
